e7d7e0e5ff78100bf7aa110a8e984a278ab2fbd4
Andrew Spittle <andrew@andrewspittle.net>
2013-09-15 15:03:10 -0700

    Remove articles_categorized_blog support from template tags.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -197,38 +197,3 @@ function articles_posted_on() {
 	);
 }
 endif;
-
-/**
- * Returns true if a blog has more than 1 category
- */
-function articles_categorized_blog() {
-	if ( false === ( $all_the_cool_cats = get_transient( 'all_the_cool_cats' ) ) ) {
-		// Create an array of all the categories that are attached to posts
-		$all_the_cool_cats = get_categories( array(
-			'hide_empty' => 1,
-		) );
-
-		// Count the number of categories that are attached to the posts
-		$all_the_cool_cats = count( $all_the_cool_cats );
-
-		set_transient( 'all_the_cool_cats', $all_the_cool_cats );
-	}
-
-	if ( '1' != $all_the_cool_cats ) {
-		// This blog has more than 1 category so articles_categorized_blog should return true
-		return true;
-	} else {
-		// This blog has only 1 category so articles_categorized_blog should return false
-		return false;
-	}
-}
-
-/**
- * Flush out the transients used in articles_categorized_blog
- */
-function articles_category_transient_flusher() {
-	// Like, beat it. Dig?
-	delete_transient( 'all_the_cool_cats' );
-}
-add_action( 'edit_category', 'articles_category_transient_flusher' );
-add_action( 'save_post',     'articles_category_transient_flusher' );
