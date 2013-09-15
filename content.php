d9a33c770ecbabb1115c70d3900dd2082094a662
Andrew Spittle <andrew@andrewspittle.net>
2013-09-15 15:23:10 -0700

    Remove articles_categorized_blog support from content template. Missed this in an earlier commit.

--- content.php
+++ content.php
@@ -20,31 +20,7 @@
 	</div><!-- .entry-summary -->
 
 	<footer class="entry-meta">
-		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'articles' ) );
-				if ( $categories_list && articles_categorized_blog() ) :
-			?>
-			<span class="cat-links">
-				<?php printf( __( 'Posted in %1$s', 'articles' ), $categories_list ); ?>
-			</span>
-			<?php endif; // End if categories ?>
-
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'articles' ) );
-				if ( $tags_list ) :
-			?>
-			<span class="tags-links">
-				<?php printf( __( 'Tagged %1$s', 'articles' ), $tags_list ); ?>
-			</span>
-			<?php endif; // End if $tags_list ?>
-		<?php endif; // End if 'post' == get_post_type() ?>
-
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'articles' ), __( '1 Comment', 'articles' ), __( '% Comments', 'articles' ) ); ?></span>
-		<?php endif; ?>
+		<?php the_tags(); ?>
 
 		<?php edit_post_link( __( 'Edit', 'articles' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-meta -->
