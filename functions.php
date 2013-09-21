7b34a30045c3a6b92749b6de127f87ab0a690312
Andrew Spittle <andrew@andrewspittle.net>
2013-09-21 11:38:31 -0700

    Exclude non-standard post formats from the homepage
    This feels totally hacky and like there's an easier way to do this. For
    now committing it as "it works"

--- functions.php
+++ functions.php
@@ -169,3 +169,42 @@ function articles_url_grabber() {
 
 	return esc_url_raw( $matches[1] );
 }
+
+/**
+ * Hide non-standard post formats from the main loop
+ * Feels hacky, probably a better way to do this...
+ */
+function exclude_non_standard_post_formats_from_loop( &$wp_query ) {
+	
+	// Do this for the home page
+	if ( $wp_query->is_home() ) {
+		
+		// Array of post formats to exclude, by slug,
+		// e.g. "post-format-{format}"
+		$post_formats_to_exclude = array(
+			'post-format-quote',
+			'post-format-aside',
+			'post-format-link',
+			'post-format-image'
+		);
+		
+		// Attach this to the $wp_query object:
+		$extra_tax_query = array(
+			'taxonomy' => 'post_format',
+			'field' => 'slug',
+			'terms' => $post_formats_to_exclude,
+			'operator' => 'NOT IN'
+		);
+		
+		$tax_query = $wp_query->get( 'tax_query' );
+		if ( is_array( $tax_query ) ) {
+			$tax_query = $tax_query + $extra_tax_query;
+		} else {
+			$tax_query = array( $extra_tax_query );
+		}
+		$wp_query->set( 'tax_query', $tax_query );
+	}
+}
+
+// Call our above action before each WP query
+add_action( 'pre_get_posts', 'exclude_non_standard_post_formats_from_loop' );
