ca8550a6bb5d6a53a9552fc91b49bf89c92e8281
Andrew Spittle <andrew@andrewspittle.net>
2013-09-22 20:50:21 -0700

    Move entry date below the main content for links
    This clears up an issue where the entry date and the link used were too
    close to each other.

--- content-link.php
+++ content-link.php
@@ -8,19 +8,17 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php articles_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
 	<div class="entry-content">
 		<?php the_content(); ?>
 	</div><!-- .entry-summary -->
 
 	<footer class="entry-meta">
+		<?php if ( 'post' == get_post_type() ) : ?>
+		<div class="entry-date">
+			<?php articles_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+		
 		<?php the_tags( '<div class="entry-tags">', ', ', '</div>' ); ?>
 	
 		<?php edit_post_link( __( 'Edit', 'articles' ), '<span class="edit-link">', '</span>' ); ?>
